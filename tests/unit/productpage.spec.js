4746ab489e64cab35b24afa1183a8698595e63ae
krishnamohanparuchuri <55274056+krishnamohanparuchuri@users.noreply.github.com>
2020-09-03 21:28:33 +0200

    intial required files

---
+++ tests/unit/productpage.spec.js
@@ -0,0 +1,16 @@
+import { shallowMount } from '@vue/test-utils'
+import ProductsPage from '@/views/ProductsPage.vue'
+
+describe('productspage.vue', () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallowMount(ProductsPage);
+    })
+
+    it('renders the text "Products"', () => {
+        const msg = 'Products';
+        const textMessage = wrapper.find('h1').text();
+        expect(textMessage).toBe(msg);
+
+    })
+})
