fa1db0864aa1bc06744eec2e171c7d9e7c93c1c0
Razieh <razie.rouzbahani@gmail.com>
2020-09-07 11:47:39 +0200

    footer and test for contactUs

--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -1,49 +1,44 @@
 <template>
   <div class="conatiner-contact">
-     <h1 class="contact-header">Contact Us</h1>
-     <form class="Contact-Form" >
-        <input required name="name" placeholder="Name" type="text" v-model="username" data-username>
-          <input required name="email" placeholder="Email" type="text">
-            <textarea name="message" rows="4" placeholder="Your Message"></textarea>
-              <button class="form-button" @click.stop.prevent="handleSubmit()">Send</button>
-  
-     </form>
-  <div class="Msg" v-if="submitted">
-     Thank you for your Message , {{ username}}
-
-   </div>
-
+    <h1 class="contact-header">Contact Us</h1>
+    <form @submit.prevent="handleSubmit" class="contact-form">
+      <input required name="name" placeholder="Name" type="text" v-model="username" data-username />
+      <input required name="email" placeholder="Email" type="text"/>
+      <textarea name="message" rows="4" placeholder="Your Message"></textarea>
+      <!-- <button class="form-button" @click.stop.prevent="handleSubmit()">Send</button> -->
+      <input type="submit">
+    </form>
+    <div class="msg" v-if="submitted">Thank you for your Message, {{ username }}</div>
   </div>
-  
-
 </template>
 
 <script>
 export default {
   name: "ContactPage",
-  
+
   data() {
     return {
-      username: '',
-      submitted: false
-    }
+      username: "",
+      submitted: false,
+    };
   },
 
-
   methods: {
-    handleSubmit(){
-       this.submitted = true
-    }
-
-  }
-
+    handleSubmit() {
+      this.submitted = true;
+    },
+  },
 };
 </script>
 
 <style>
-
-.Contact-Form{
-  display:grid;
+.conatiner-contact {
+  background: #50a280;
+  width: 100%;
+  height: 600px;
+}
+.contact-form {
+  display: grid;
   grid-template-columns: auto;
   grid-auto-flow: row;
   grid-gap: 8px;
@@ -51,34 +46,31 @@ export default {
   padding: 30px;
   border: 2px solid lightslategray;
   border-radius: 8px;
+  width: 60%;
+  margin: 0 auto;
 }
-.form-button{
-    color:black;
-    cursor: pointer;
-    border-radius: 6px;
-    font-weight: bold;
-    width: 100px;
+.form-button {
+  color: black;
+  cursor: pointer;
+  border-radius: 6px;
+  font-weight: bold;
+  width: 100px;
 }
 
-.Contact-Form > input,
-.Contact-Form  >textarea,
-.Contact-Form > button {
-
-  
+.contact-form > input,
+.contact-form > textarea,
+.contact-form > button {
   grid-row: auto;
-  border:rgb(88, 81, 81);
+  border: rgb(88, 81, 81);
   padding: 10px;
 }
 
 .form-button:hover {
   background-color: limegreen;
-
 }
-.Msg{
-
-  color:black;
-  font-family: Georgia, 'Times New Roman', Times, serif;
+.msg {
+  color: black;
+  font-family: Georgia, "Times New Roman", Times, serif;
   font-weight: bolder;
-} 
-
+}
 </style>
