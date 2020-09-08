daf3411123bdd04cb9feed1da7ccd2049662e0d5
krishnamohanparuchuri <55274056+krishnamohanparuchuri@users.noreply.github.com>
2020-09-08 23:51:31 +0200

    modify about page

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about-container">
-    <h1>This is a about page</h1>
+    <h1>Buy the best value Products</h1>
     <div class="wrapper">
       <img class="Logo" src="../assets/shopLogo.png" alt="logo" />
       <h1 class="title">{{ title }}</h1>
@@ -11,19 +11,19 @@
 </template>
 
 <script>
-import Footer from '@/components/Footer.vue'
+import Footer from "@/components/Footer.vue";
 export default {
   name: "Aboutpage",
   components: {
-    Footer
+    Footer,
   },
   data() {
     return {
       title: "About us",
       description:
-        "If you would like to experience the best of online shopping for men, women and kids , you are at the right place. MyShop is the ultimate destination for fashion and lifestyle, being host to a wide array of merchandise including clothing, footwear, accessories, jewellery, personal care products and more. It is time to redefine your style statement with our treasure-trove of trendy items. Our online store brings you the latest in designer products straight out of fashion houses. You can shop online at MyShop from the comfort of your home and get your favourites delivered right to your doorstep."
+        "If you would like to experience the best of online shopping for men, women and kids , you are at the right place. MyShop is the ultimate destination for fashion and lifestyle, being host to a wide array of merchandise including clothing, footwear, accessories, jewellery, personal care products and more. It is time to redefine your style statement with our treasure-trove of trendy items. Our online store brings you the latest in designer products straight out of fashion houses. You can shop online at MyShop from the comfort of your home and get your favourites delivered right to your doorstep.",
     };
-  }
+  },
 };
 </script>
 
@@ -51,12 +51,9 @@ export default {
   font-weight: bold;
   margin: 100px 0;
   text-align: center;
-  height: 70vh; 
+  height: 70vh;
 }
-.Logo{
-
-  height:80px;
-
-
+.Logo {
+  height: 80px;
 }
 </style>
